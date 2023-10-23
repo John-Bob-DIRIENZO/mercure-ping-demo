d65a903d2d30c31b639f5059be03f12d3cd5ac8f
Zalerys <gustaveconstans@gmail.com>
2023-10-23 05:11:53 +0200

    feat(main): add route chat all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NeedAuth from "./Auth/NeedAuth";
 import UserList from "./Component/UserList";
 import Login from "./Auth/Login";
 import UserProvider from "./Context/UserContext";
+import ChatAll from "./Component/ChatAll";
 
 function App() {
-    return (
-        <UserProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={
-                        <NeedAuth>
-                            <UserList/>
-                        </NeedAuth>
-                    }/>
-                    <Route path='/login' element={<Login/>}/>
-                </Routes>
-            </BrowserRouter>
-        </UserProvider>
-    );
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <NeedAuth>
+                <UserList />
+              </NeedAuth>
+            }
+          />
+          <Route
+            path="/all"
+            element={
+              <NeedAuth>
+                <ChatAll />
+              </NeedAuth>
+            }
+          />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
 
 export default App;
